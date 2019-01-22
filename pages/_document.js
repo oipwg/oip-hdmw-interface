86a47376f336a6c500d80754e3dea4745c6161d6
orpheus <ryanachacon@gmail.com>
2019-01-22 13:32:33 -0800

    add title to document head

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
 		return (
 			<html lang="en" style={{height: '100%'}}>
 			<Head>
+				<title>OIP HDMW</title>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="google" content="notranslate" />
