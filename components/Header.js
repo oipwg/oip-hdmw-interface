a701a6d67f7790f411e580e677f7fce08bf74c30
orpheus <ryanachacon@gmail.com>
2019-01-22 15:33:07 -0800

    cleanup header

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
 
-
 const styles = theme => ({
 	root: {
 		height: '70px',
@@ -40,47 +39,6 @@ const styles = theme => ({
 		fontWeight: 'bold',
 		margin: '0px 20px 0px auto',
 	},
-	// search: {
-	// 	position: 'relative',
-	// 	borderRadius: theme.shape.borderRadius,
-	// 	backgroundColor: fade(theme.palette.common.white, 0.15),
-	// 	'&:hover': {
-	// 		backgroundColor: fade(theme.palette.common.white, 0.25),
-	// 	},
-	// 	marginRight: theme.spacing.unit * 2,
-	// 	marginLeft: 0,
-	// 	width: '100%',
-	// 	[theme.breakpoints.up('sm')]: {
-	// 		marginLeft: theme.spacing.unit * 3,
-	// 		width: 'auto',
-	// 	},
-	// },
-	// searchIcon: {
-	// 	width: theme.spacing.unit * 9,
-	// 	height: '100%',
-	// 	position: 'absolute',
-	// 	pointerEvents: 'none',
-	// 	display: 'flex',
-	// 	alignItems: 'center',
-	// 	justifyContent: 'center',
-	// },
-	// inputRoot: {
-	// 	color: 'inherit',
-	// 	width: '100%',
-		// borderLeft: '0.1em solid #d3d3d3',
-		// borderRight: '0.1em solid #d3d3d3',
-	// },
-	// inputInput: {
-	// 	paddingTop: theme.spacing.unit,
-	// 	paddingRight: theme.spacing.unit,
-	// 	paddingBottom: theme.spacing.unit,
-	// 	paddingLeft: theme.spacing.unit * 10,
-	// 	transition: theme.transitions.create('width'),
-	// 	width: '100%',
-	// 	[theme.breakpoints.up('md')]: {
-	// 		width: 200,
-	// 	},
-	// },
 	sectionDesktop: {
 		display: 'none',
 		[theme.breakpoints.up('md')]: {
@@ -96,7 +54,7 @@ const styles = theme => ({
 });
 
 function Header(props) {
-	const {classes, redirectUrl} = props;
+	const {classes} = props;
 
 	return (
 		<div className={classes.root}>
@@ -109,25 +67,6 @@ function Header(props) {
 					</Typography>
 
 					<div className={classes.grow}/>
-					{/*<div className={classes.search}>*/}
-						{/*<div className={classes.searchIcon}>*/}
-							{/*<SearchIcon/>*/}
-						{/*</div>*/}
-						{/*<InputBase*/}
-							{/*placeholder="Search…"*/}
-							{/*classes={{*/}
-								{/*root: classes.inputRoot,*/}
-								{/*input: classes.inputInput,*/}
-							{/*}}*/}
-						{/*/>*/}
-					{/*</div>*/}
-					{/*<div className={classes.grow}/>*/}
-
-					{/*<div className={classes.sectionDesktop}>*/}
-						{/*<Link prefetch href={{pathname: '/public/login', query: {redirectUrl}}}>*/}
-							{/*<a className={classes.loginLink}>Login</a>*/}
-						{/*</Link>*/}
-					{/*</div>*/}
 					<div className={classes.sectionMobile}>
 						{/*...*/}
 					</div>
