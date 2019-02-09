f2d75a929bf2f1ffe749b236c2135158c75b3b34
orpheus <ryanachacon@gmail.com>
2019-02-08 20:01:53 -0800

    fix: toggle more text data on overflow

--- components/views/dumb/FloDataContainer.js
+++ components/views/dumb/FloDataContainer.js
@@ -1,32 +1,26 @@
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 
 function FloDataContainer({floData, classes}) {
-	const floDataRef = useRef(null)
 	const [toggled, toggle] = useState(false)
 	
-	const getTextLen = () => {
-		if (!floDataRef.current) {
-			return false
-		}
-		return floDataRef.current.innerText.length > 70
-	}
-	
-	let text = toggled ? 'show less' : 'show more'
-	
-	const toggleText = () => {
+	const handleToggle = () => {
 		toggle(!toggled)
 	}
 	
+	const text = toggled ? 'show less' : 'show more'
+	
 	const getElement = () => {
-		console.log(floDataRef.current)
-		return <span onClick={toggleText}>{text}</span>
+		return <span onClick={handleToggle}>{text}</span>
 	}
 	
-	const showMoreText = getTextLen() ? getElement() : null
-	
+	const longFloData = () => {
+		return floData.length > 70
+	}
+	const toggleTextOverflow = longFloData() ? getElement() : null
+
 	return <div className={classes.floDataContainer} style={{whiteSpace: toggled ? 'unset' : 'nowrap'}}>
-		<p ref={floDataRef}>floData: {floData}</p>
-		{showMoreText}
+		<p>floData: {floData}</p>
+		{toggleTextOverflow}
 	</div>
 }
 
