39697f5d77fe60ed8ef18b2f9f1b747465dbb424
orpheus <ryanachacon@gmail.com>
2019-02-12 14:01:29 -0800

    add bold font to active display header  link

--- components/views/dumb/DisplayHeader.js
+++ components/views/dumb/DisplayHeader.js
@@ -7,20 +7,31 @@ import styles from '../../../styles/views/dumb/DisplayHeader'
 function DisplayHeader(props) {
 	const {classes, setActiveView, activeView} = props
 	
+	const getActiveLinkStyle = (view) => {
+		if (activeView === view) {
+			return {fontWeight: 'bold'}
+		}
+		return {}
+	}
+	
 	return <div className={classes.displayHeader}>
 		<span
+			style={getActiveLinkStyle('addresses')}
 			onClick={() => setActiveView('addresses')}
 			className={classes.viewLink}>Addresses
 		</span>
 		<span
+			style={getActiveLinkStyle('transactions')}
 			onClick={() => setActiveView('transactions')}
 			className={classes.viewLink}>Transactions
 		</span>
 		<span
+			style={getActiveLinkStyle('send')}
 			onClick={() => setActiveView('send')}
 			className={classes.viewLink}>Send
 		</span>
 		<span
+			style={getActiveLinkStyle('settings')}
 			onClick={() => setActiveView('settings')}
 			className={classes.viewLink}>Settings
 		</span>
